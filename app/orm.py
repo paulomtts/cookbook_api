2669280847ebe4585ea73525a345102d67bf31b0
paulomtts <paulomtts@outlook.com>
2023-11-25 14:53:20 -0300

    various fixes and greatly improved syntax and returning parsing

--- app/orm.py
+++ app/orm.py
@@ -1,6 +1,6 @@
 from fastapi import status
 from sqlmodel import and_
-from sqlalchemy import create_engine, insert, delete, update, inspect
+from sqlalchemy import create_engine, select, insert, delete, update, inspect, Column
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects.postgresql import insert as postgres_upsert
 from sqlalchemy.exc import IntegrityError, InternalError, OperationalError, ProgrammingError
@@ -8,30 +8,34 @@ from sqlalchemy.orm.exc import StaleDataError
 from collections import namedtuple
 
 from pydantic import BaseModel
-from typing import List, Dict, Literal, Union, Callable, Optional
+from typing import List, Literal, Union, Callable, Optional, Any
+import pandas as pd
+
+
 
 class Task(BaseModel):
     function: Callable
     args: Optional[List] = None
-    kwargs: Optional[Dict] = None
+    mapping_cls: Optional[Any] = None
 
-    def __init__(self, function: Callable, args: Optional[List] = None, kwargs: Optional[Dict] = None):
-        super().__init__(function=function, args=args, kwargs=kwargs)
+    def __init__(self, function: Callable, args: Optional[List] = None, mapping_cls: Optional[Any] = None):
+        super().__init__(function=function, args=args, mapping_cls=mapping_cls)
 
         if args is None:
             self.args = []
-        if kwargs is None:
-            self.kwargs = {}
 
     def __iter__(self):
         yield self.function
         yield self.args
-        yield self.kwargs
+        yield self.mapping_cls
 
 class Result(BaseModel):
-    content: list
+    content: List[pd.DataFrame]
     status_code: Literal[200, 204, 400, 500, 503]
     client_message: str
+
+    class Config:
+        arbitrary_types_allowed = True
 
     def __init__(self, content: list, status_code: Literal[200, 204, 400, 500, 503], client_message: str):
         super().__init__(content=content, status_code=status_code, client_message=client_message)
@@ -96,6 +100,12 @@ ERROR_MAP = {
         , "One or more rows involved in the operation did could not be found or did not match the expected values."
     )
 
+    , IndexError: ErrorObject(
+        status.HTTP_400_BAD_REQUEST
+        , "Index error."
+        , "Expected returning data but none was found."
+    )
+
     , Exception: ErrorObject(
         status.HTTP_500_INTERNAL_SERVER_ERROR
         , "Internal server error."
@@ -116,48 +126,88 @@ class DBManager():
 
     def __del__(self):
         """
-        Automatically close the session and release resources when the DBClient object is about to be destroyed.
+        Automatically close the session and release resources when this object is about to be destroyed.
         """
         try:
-            self.session.rollback() # ATTENTION: not yet tested
-            self.logger.info(f"Uncommitted changes were rolled back.")
             self.session.close()
+            self.logger.info(f"Gracefully closed a session.")
         except AttributeError:
             pass  # In case close() method is already called or doesn't exist
 
 
-    def query(self, table_cls, filters: list = None, messages: Messages = None, order_by = None):
+    def query(self, table_cls, filters: list = None, messages: Messages = None, order_by: List[Column] = None, as_task_list: bool = False):
         """
-        Query and returns rows from a table. If no filters are passed, all rows are returned.
+        Executes a query on the specified table class with optional filters and ordering.
+
+        Args:
+            - table_cls (`class`): The table class to query.
+            - filters (`list, optional`): A dictionary containing the column names as keys and the values to filter on as values.
+            - messages (`Messages, optional`): An object for storing success messages. Defaults to None.
+            - order_by (`bool, optional`): A list of columns to order the query by, passed as as <instance>.<column_name>. Defaults to None.
+            - as_task_list (`bool, optional`): Whether to avoid immediately executing the task and return it as a callback instead. Defaults to `False`.
+
+        Returns:
+            `Task` or `Result`: `Task` object or a `Result` object containing the query results.
         """
+
         if filters is None: filters = []
 
+        statement = select(table_cls).where(*[getattr(table_cls, column_name).in_(values) for column_name, values in filters.items()])
+
         if order_by:
-            fn = lambda filters: self.session.query(table_cls).filter(and_(*filters)).all()
-            task = Task(fn, [filters])
-        else:
-            fn = lambda filters, order_by: self.session.query(table_cls).filter(and_(*filters)).order_by(order_by).all()
-            task = Task(fn, [filters, order_by])
+            statement = statement.order_by(*order_by)
+
+        fn = lambda statement: self.session.execute(statement)
+        task = Task(fn, [statement], mapping_cls=table_cls)
+
+        if as_task_list:
+            return task
 
         return self.touch(task, messages, True)
 
 
-    def insert(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True):
+    def insert(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True, as_task_list: bool = False):
         """
-        Insert a list of dictionaries as rows in a table.
+        Insert data into the specified table.
+
+        Args:
+            - table_cls (`Table`): The table class to insert data into.
+            - data_list (`List[dict]`): A list of dictionaries representing the data to be inserted.
+            - messages (`Messages`, optional): An object for storing success messages. Defaults to `None`.
+            - returning (`bool`, optional): Whether to include the inserted data in the result. Defaults to `True`.
+            - as_task_list (`bool`, optional): Whether to avoid immediately executing the task and return it as a callback instead. Defaults to `False`.
+
+        Returns:
+            `Task`, `List[Task]` or `Result`: The task or list of tasks representing the insert operation, or a `Result` object containing the inserted data.
         """
         statement = insert(table_cls).values(data_list)
 
         if returning:
-            statement = statement.returning(table_cls.__table__.columns)
+            statement = statement.returning(table_cls)
 
         fn = lambda statement: self.session.execute(statement)
-        task = Task(fn, [statement])
+        task = Task(fn, [statement], mapping_cls=table_cls)
 
-        return self.touch(task, messages, no_parse=(not returning))
+        if as_task_list:
+            return task
+
+        return self.touch(task, messages)
 
 
-    def update(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True):
+    def update(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True, as_task_list: bool = False):
+        """
+        Update records in the database table.
+
+        Args:
+            - table_cls (`class`): The class representing the database table.
+            - data_list (`List[dict]`): A list of dictionaries containing the updated data for each record.
+            - messages (`Messages, optional`): An object for storing success messages. Defaults to None.
+            - returning `(bool, optional`): Whether to return the updated records. Defaults to True.
+            - as_task_list (`bool, optional`): Whether to avoid immediately executing the task and return it as a callback instead. Defaults to `False`.
+
+        Returns:
+           ` Union[List[Task], Any]`: If `as_task_list` is True, returns a list of update tasks. Otherwise, returns the result of `self.touch`.
+        """
 
         assert isinstance(data_list, list), f"Data must be type <list>. Instead, it is type <{type(data_list).__name__}>."
 
@@ -172,44 +222,65 @@ class DBManager():
                 update(table_cls)
                 .where(*conditions)
                 .values(data)
-                
             )
 
             if returning:
-                statement = statement.returning(table_cls.__table__.columns)
+                statement = statement.returning(table_cls)
 
             fn = lambda statement: self.session.execute(statement)
-            task = Task(fn, [statement])
+            task = Task(fn, [statement], mapping_cls=table_cls)
             task_list.append(task)
         
-        return self.touch(task_list, messages, no_parse=(not returning))
+        if as_task_list:
+            return task_list
+        
+        return self.touch(task_list, messages)
 
 
-    def delete(self, table_cls, filters: dict, messages: dict = None, returning: bool = True):
+    def delete(self, table_cls, filters: dict, messages: dict = None, returning: bool = True, as_task_list: bool = False):
         """
-        Delete a list of ORM objects. This method uses Postgres' RETURNING clause to return the deleted objects.
+        Delete records from the specified table based on the provided filters.
+
+        Args:
+            table_cls (`class`): The table class representing the table to delete records from.
+            filters (`dict`): A dictionary containing the column names as keys and the values to filter on as values.
+            messages (`dict, optional`): A dictionary containing messages to be printed if succesful. Defaults to None.
+            returning (`bool, optional`): Whether to include the deleted records in the return value. Defaults to True.
+            as_task_list (`bool, optional`): Whether to avoid immediately executing the task and return it as a callback instead. Defaults to `False`.
+
+        Returns:
+            `Task` or `list`: The deletion task or a `list` of deletion tasks, depending on the value of `as_task_list`.
         """
         conditions = [getattr(table_cls, column_name).in_(values) for column_name, values in filters.items()]
         statement = delete(table_cls).where(*conditions)
         
         if returning:
-            statement = statement.returning(table_cls.__table__.columns)
+            statement = statement.returning(table_cls)
 
         fn = lambda statement: self.session.execute(statement)
-        task = Task(fn, [statement])
+        task = Task(fn, [statement], mapping_cls=table_cls)
 
-        return self.touch(task, messages, no_parse=(not returning))
+        if as_task_list:
+            return task
+
+        return self.touch(task, messages)
 
 
-    def upsert(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True):
+    def upsert(self, table_cls, data_list: List[dict], messages: Messages = None, returning: bool = True, as_task_list: bool = False):
         """
-        Attempt to insert a list of dictionaries as rows into a table. If there is 
-        a conflict with the primary key, update them instead. This method uses 
-        Postgres' ON CONFLICT clause to perform the upsert.
+        Attempts to insert data into the specified table, and updates the data if the insert fails because of a unique constraint violation.
+
+        Args:
+            table_cls (`class`): The table class.
+            data_list (`List[dict]`): A list of dictionaries containing the data to be upserted.
+            messages (`Messages`, optional): A dictionary containing messages to be printed if succesful. Defaults to None.
+            returning (`bool`, optional): Whether to return the upserted data. Defaults to True.
+            as_task_list (`bool`, optional): Whether to avoid immediately executing the task and return it as a callback instead. Defaults to `False`.
+
+        Returns:
+            `Union[List[Task], Any]`: If `as_task_list` is True, returns a list of Task objects representing the upsert tasks.
+            Otherwise, returns the result of the `touch` method.
         """
-
-        assert isinstance(data_list, list), f"Data must be type <list>. Instead, it is type <{type(data_list).__name__}>."
-
         task_list = []
 
         for data in data_list:
@@ -217,17 +288,19 @@ class DBManager():
                         .on_conflict_do_update(index_elements=[table_cls.id], set_=data)\
             
             if returning:
-                statement = statement.returning(table_cls.__table__.columns)
-                        
+                statement = statement.returning(table_cls)
             
             fn = lambda statement: self.session.execute(statement)
-            task = Task(fn, [statement])
+            task = Task(fn, [statement], mapping_cls=table_cls)
             task_list.append(task)
 
-        return self.touch(task_list, messages, no_parse=(not returning))
+        if as_task_list:
+            return task_list
+
+        return self.touch(task_list, messages)
 
 
-    def touch(self, task_list: Union[Task, List[Task]], messages: Messages = None, is_select=False, no_parse = False) -> Result:
+    def touch(self, task_list: Union[Task, List[Task]], messages: Messages = None, is_select=False, mapping_cls = None) -> Result:
         """
         Perform a single transaction. This method is used to wrap all CRUD operations.
         Autocommit is enabled by default, but can be disabled by setting commit=False.
@@ -236,10 +309,9 @@ class DBManager():
         or a list of tuples if multiple functions are passed.
         """
 
-        # assert (messages is None or isinstance(messages, Messages)), "Messages must be None or a namedtuple containing 'client' and 'logger' messages."
         if type(task_list) != list: task_list = [task_list]
-        
-        content = []
+       
+        content_list = []
 
         client_message = 'Operation successful.'
         if messages and messages.client:
@@ -247,48 +319,47 @@ class DBManager():
 
         try:
             for task in task_list:
+                task = task if isinstance(task, list) else [task]
 
-                fn, args, kwargs = task
-                value = fn(*args, **kwargs)
+                group_content = []
+                for subtask in task:
+                    fn, args, mapping_cls = subtask
+                    content = fn(*args).fetchall()
 
-                if len(task_list) == 1 and is_select and getattr(value, 'empty', True): # getattr: allow non-Pandas queries
-                    self.logger.warning(f"Table was found but had no rows.")
-                    return [], STATUS_DICT[204], "The resource was found but had no data stored."
+                    parsed_content = []
+                    for row in content:
+                        dct = dict(row[0])
+                        dct.pop('_sa_instance_state', None)
+                        parsed_content.append(dct)
 
-                if messages and messages.logger: 
-                    self.logger.debug(messages.logger)
+                    group_content.extend(parsed_content)
 
-                content.append(value)
+                df = pd.DataFrame(group_content)
+
+                if mapping_cls:
+                    mapping_columns = mapping_cls.__annotations__.keys()
+                    columns = [*mapping_columns] + [col for col in df.columns if col not in mapping_columns]
+                    df = df[columns]
+
+                content_list.append(df)
 
             if not is_select:
                 self.session.commit()
 
-        except (IntegrityError, ProgrammingError, OperationalError, InternalError, ValueError, StaleDataError, Exception) as e:
+        except (IntegrityError, ProgrammingError, OperationalError, InternalError, ValueError, StaleDataError, IndexError, Exception) as e:
             self.session.rollback()
 
             error = ERROR_MAP.get(type(e))
             self.logger.error(f"{error.logger_message} Message:\n\n {e}.\n")
 
             return Result([], error.status_code, error.client_message)
+
+        if is_select and len(content_list) == 1 and getattr(content_list[0], 'empty', True):
+            self.logger.debug(f"Query returned no results.")
+            return Result([], STATUS_DICT[204], client_message)
         
-        if no_parse:
-            return Result([], STATUS_DICT[200], client_message)
+        if messages and messages.logger: 
+            self.logger.debug(messages.logger)
 
-        parsed_content = self._parse_returning(content)
-        return Result(parsed_content, STATUS_DICT[200], client_message)
+        return Result(content_list, STATUS_DICT[200], client_message)
     
-
-    def _parse_returning(self, content: list):
-        """
-        Parse the result of a query with a RETURNING clause.
-        """
-        assert isinstance(content, list), f"Content must be type <list>. Instead, it is type <{type(content).__name__}>."
-
-        parsed_content = []
-        for row in content:
-            keys = [key for key in row.keys()]
-            values = [str(value) for value in row.fetchall()[0]]
-
-            parsed_content.append(dict(zip(keys, values)))
-
-        return parsed_content
--- main.py
+++ main.py
@@ -26,13 +26,19 @@ async def azuretest():
     new_ingredient_4 = dict(name='Something else', description="This is a test ingredient", type='Test', updated_at=datetime.datetime.now())
     new_ingredient_5 = dict(name='Another test ingredient', description="This is a test ingredient", type='Test', updated_at=datetime.datetime.now())
 
-    # results = db.insert(Ingredients, [new_ingredient_4, new_ingredient_5], Messages(client='Submission successful!', logger='Insert successful'), returning=False)
-    # results = db.update(Ingredients, [new_ingredient_1, new_ingredient_2], Messages(client='Submission successful!', logger='Update successful'), returning=False)
-    # results = db.upsert(Ingredients, [new_ingredient_1, new_ingredient_3], Messages(client='Submission successful!', logger='Upsert successful'), returning=False)
-    results = db.delete(Ingredients, {'id': [3]}, Messages(client='Submission successful!', logger='Delete successful'), returning=False)
+    task_list = [
+        db.insert(Ingredients, [new_ingredient_4, new_ingredient_5], as_task_list=True)
+        , db.update(Ingredients, [new_ingredient_1, new_ingredient_2], as_task_list=True)
+        , db.upsert(Ingredients, [new_ingredient_1, new_ingredient_3], as_task_list=True)
+        , db.delete(Ingredients, {'id': [3]}, as_task_list=True)
+        , db.query(Ingredients, {'id': [2, 99]}, as_task_list=True)
+    ]
 
-    print(results.content)
-    
+    results = db.touch(task_list, Messages(client='Submission successful!', logger='Nested operations succeeded!'))
+
+    for df in results.content:
+        print(df)
+
     return JSONResponse(status_code=200, content={"message": "healthy."})
 
 
