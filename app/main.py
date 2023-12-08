626317bd2f387289c3b87d125d847cf630bb9455
paulomtts <paulomtts@outlook.com>
2023-12-08 02:24:44 -0300

    fixed file structuring and imports

--- app/main.py
+++ app/main.py
@@ -1,14 +1,12 @@
 from fastapi.responses import JSONResponse
-import uvicorn
-import sys
-import os
-sys.path.append(os.getcwd())
-
 
 from core.start import app 
 from core.crud import crud_router
 from core.auth import auth_router
 from custom.custom import customRoutes_router
+
+import uvicorn
+
 
 app.include_router(crud_router)
 app.include_router(auth_router)
