69cb01952023403e9b5e51ce22ba7a5f59bd6a8f
paulomtts <paulomtts@outlook.com>
2023-12-12 14:03:02 -0300

    file restructuring, users crud only own data

--- src/core/auth.py
+++ src/core/auth.py
@@ -27,10 +27,12 @@ auth_router = APIRouter()
 # payload is valid, the server can then verify the user's identity and
 # allow the user to access the protected route.
 
+############# DEVELOPMENT ONLY #############
 GOOGLE_CLIENT_ID = os.getenv('GOOGLE_CLIENT_ID')
 GOOGLE_CLIENT_SECRET = os.getenv('GOOGLE_CLIENT_SECRET')
 GOOGLE_REDIRECT_URI = os.getenv('GOOGLE_REDIRECT_URI')
 FRONTEND_REDIRECT_URI = os.getenv('FRONTEND_REDIRECT_URL')
+############# DEVELOPMENT ONLY #############
 
 class MissingSessionError(BaseException):
     """
